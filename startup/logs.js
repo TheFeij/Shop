de05ab664a3d566d9c07b4438b13d4a9834cac3a
Abolfazl Moradi Feijani <abolfazl.moradi.feijani@gmail.com>
2023-07-21 21:50:17 +0330

    2nd commit: added route files. added default.json in config foulder to store configurations. also added the logs startup file. logs.js adds console, file and mongodb transports to winston. also added two event listeners, one for "uncaughtException" and the other for "unhandledRejection" to catch unhandled errors in the program

---
+++ startup/logs.js
@@ -0,0 +1,32 @@
+const config = require("config")
+const winston = require("winston")
+require("winston-transport")
+require("winston-mongodb")
+
+
+
+module.exports = function(){
+
+    // adding console, file and mongodb transports to winston
+    winston.add(new winston.transports.Console())
+    winston.add(new winston.transports.File({filename: "logsFile.log"}))
+    winston.add(new winston.transports.MongoDB({
+        db: config.get("Database.connectionString"),
+        level: "info"
+    }))
+
+
+    // catching uncaught exceptions in the app outside the scope of express
+    // (exceptions in the scope of express will be handled by a middleware function)
+    process.on("uncaughtException", (ex) => {
+        winston.log(ex.message, ex)
+        process.exit(1)
+    })
+
+    // catching unhandled promise rejections in the app outside the scope of express
+    // (rejections in the scope of express will be handled by a middleware function)
+    process.on("uncaughtException", (ex) => {
+        winston.log(ex.message, ex)
+        process.exit(1)
+    })
+}
