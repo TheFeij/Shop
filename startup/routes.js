94887f24eed1683a5147b1b2c23b8b2108e557b4
Abolfazl Moradi Feijani <abolfazl.moradi.feijani@gmail.com>
2023-07-22 21:50:10 +0330

    added login and refresh-token router to the express

--- startup/routes.js
+++ startup/routes.js
@@ -2,6 +2,8 @@ const express = require("express")
 const homeRouter = require("../routes/home")
 const signupRouter = require("../routes/signup")
 const verifyRouter = require("../routes/verify")
+const loginRouter = require("../routes/login")
+const refreshTokenRouter = require("../routes/refresh-token")
 
 // a function to add all the routers to express
 module.exports = function(app){
@@ -9,4 +11,6 @@ module.exports = function(app){
     app.use("/", homeRouter)
     app.use("/signup", signupRouter)
     app.use("/verify", verifyRouter)
+    app.use("/login", loginRouter)
+    app.use("/refresh-token", refreshTokenRouter)
 }
