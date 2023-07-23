ac74bf420d885b10adf44e4923ac211b4191bc6e
Abolfazl Moradi Feijani <abolfazl.moradi.feijani@gmail.com>
2023-07-23 21:02:57 +0330

    removed a white space

--- models/user.js
+++ models/user.js
@@ -52,7 +52,7 @@ const userSchema = new mongoose.Schema({
  */
 userSchema.methods.hashPassword = async function(){
     const salt = await bcrypt.genSalt(10)
-    this.password  = await bcrypt.hash(this.password, salt)
+    this.password = await bcrypt.hash(this.password, salt)
 }
 
 /**
