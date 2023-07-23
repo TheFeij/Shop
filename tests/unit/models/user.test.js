a5d41c02f3b83d9524d1c3f194b9d0b60cfd1c42
Abolfazl Moradi Feijani <abolfazl.moradi.feijani@gmail.com>
2023-07-23 21:02:24 +0330

    created unit a test for hashPassword method. also removed semicolons from test files

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -1,32 +1,36 @@
 require('dotenv').config();
 const jwt = require("jsonwebtoken")
 const {UserModel} = require("../../../models/user")
+const bcrypt = require("bcrypt")
+
+
+
+// Creating a test user object
+const user = new UserModel({
+    firstname: "firstname",
+    lastname: "lastname",
+    email: "test@email.com",
+    password: "password",
+});
 
 
 
 describe("generateAccessToken", () => {
-    // Creating a test user object
-    const user = new UserModel({
-        firstname: "firstname",
-        lastname: "lastname",
-        email: "test@email.com",
-        password: "password",
-    });
 
     it("should return a valid JWT with correct payload", () => {
         // Generating an access token
-        const accessToken = user.generateAccessToken();
+        const accessToken = user.generateAccessToken()
 
         // Verifying the access token
-        const decoded = jwt.verify(accessToken, process.env.JWT_PRIVATE_KEY);
+        const decoded = jwt.verify(accessToken, process.env.JWT_PRIVATE_KEY)
 
         // Expecting the decoded token to contain the user's _id as a string
-        expect(decoded).toMatchObject({ id: user._id.toString() });
+        expect(decoded).toMatchObject({ id: user._id.toString() })
     })
 
     it("should return a valid JWT with the correct expiration time", () => {
         // Generating the access token using the method
-        const accessToken = user.generateAccessToken();
+        const accessToken = user.generateAccessToken()
 
         // Verifying the access token
         const decoded = jwt.verify(accessToken, process.env.JWT_PRIVATE_KEY);
@@ -39,70 +43,51 @@ describe("generateAccessToken", () => {
         expect(expirationTime).toBeGreaterThan(currentTime);
         expect(expirationTime).toBeCloseTo(currentTime + 10 * 60);
     })
-
-    it.each([
-        null,
-        undefined,
-        NaN,
-        false,
-        0,
-        "",])
-    ("should throw an error for invalid input: %p", (invalidInput) => {
-        // Calling the method with invalid input should throw an error
-        expect(() => {
-            UserModel.generateAccessToken(invalidInput);
-        }).toThrow();
-    });
 })
 
 describe("generateRefreshToken", () => {
-    // Creating a test user object
-    const user = new UserModel({
-        firstname: "firstname",
-        lastname: "lastname",
-        email: "test@email.com",
-        password: "password",
-    });
 
     it("should return a valid JWT with correct payload", () => {
         // Generating a refresh token
-        const refreshToken = user.generateRefreshToken();
+        const refreshToken = user.generateRefreshToken()
 
         // Verifying the refresh token
-        const decoded = jwt.verify(refreshToken, process.env.JWT_PRIVATE_KEY);
+        const decoded = jwt.verify(refreshToken, process.env.JWT_PRIVATE_KEY)
 
         // Expecting the decoded token to contain the user's _id as a string
-        expect(decoded).toMatchObject({ id: user._id.toString() });
+        expect(decoded).toMatchObject({ id: user._id.toString() })
     })
 
     it("should return a valid JWT with the correct expiration time", () => {
         // Generating the refresh token using the method
-        const refreshToken = user.generateRefreshToken();
+        const refreshToken = user.generateRefreshToken()
 
         // Verifying the refresh token
-        const decoded = jwt.verify(refreshToken, process.env.JWT_PRIVATE_KEY);
+        const decoded = jwt.verify(refreshToken, process.env.JWT_PRIVATE_KEY)
 
         // Refresh token should expire in 12 hours
-        const currentTime = Math.floor(Date.now() / 1000);
-        const expirationTime = decoded.exp;
+        const currentTime = Math.floor(Date.now() / 1000)
+        const expirationTime = decoded.exp
 
         // Expecting the expiration time to be 12 hours
-        expect(expirationTime).toBeGreaterThan(currentTime);
-        expect(expirationTime).toBeCloseTo(currentTime + 12 * 60 * 60);
+        expect(expirationTime).toBeGreaterThan(currentTime)
+        expect(expirationTime).toBeCloseTo(currentTime + 12 * 60 * 60)
     })
+})
 
-    it.each([
-        null,
-        undefined,
-        NaN,
-        false,
-        0,
-        "",])
-    ("should throw an error for invalid input: %p", (invalidInput) => {
-        // Calling the method with invalid input should throw an error
-        expect(() => {
-            UserModel.generateRefreshToken(invalidInput);
-        }).toThrow();
-    });
+describe("hashPassword", () => {
+
+    it("should hash password correctly", async () => {
+        // getting the original password
+        const plainTextPassword = user.password
+        // hashing user.password, so from now on user.password is hashed
+        await user.hashPassword()
+
+        // Check if the original password and the hashed one are the same or not
+        const result = await bcrypt.compare(plainTextPassword, user.password)
+
+        // expect the result to be true
+        expect(result).toBe(true)
+    })
 })
 
