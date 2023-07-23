2930dda1688e18bc3aa993d88e872ccd91e40d96
Abolfazl Moradi Feijani <abolfazl.moradi.feijani@gmail.com>
2023-07-23 11:20:22 +0330

    added the products router to the express app

--- startup/routes.js
+++ startup/routes.js
@@ -4,6 +4,7 @@ const signupRouter = require("../routes/signup")
 const verifyRouter = require("../routes/verify")
 const loginRouter = require("../routes/login")
 const refreshTokenRouter = require("../routes/refresh-token")
+const productsRouter = require("../routes/products")
 
 // a function to add all the routers to express
 module.exports = function(app){
@@ -13,4 +14,5 @@ module.exports = function(app){
     app.use("/verify", verifyRouter)
     app.use("/login", loginRouter)
     app.use("/refresh-token", refreshTokenRouter)
+    app.use("/products", productsRouter())
 }
