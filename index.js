b124788635a697f361ea7fe96dddf68756c9abcc
Abolfazl Moradi Feijani <abolfazl.moradi.feijani@gmail.com>
2023-07-23 13:34:51 +0330

    cleaned and improved comments of the index.js file

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const express = require("express")
-const app = express()
-const winston = require("winston")
-const fs = require("fs")
-const https = require("https")
+// Import required modules and libraries
+const express = require("express");     // Express.js web framework
+const app = express();                  // An instance of the Express application
+const winston = require("winston");     // Logger library for logging
+const fs = require("fs");               // File system module for file operations
+const https = require("https");         // HTTPS module for creating secure server
 
 
+
+// Load environment variables from the .env file using 'dotenv' package
 require('dotenv').config();
+// Set up application routes by importing and calling the 'routes' module
 require("./startup/routes")(app)
+// Initialize logging configuration to capture application logs
 require("./startup/logs")()
+// Connect to the database and set up models and schemas
 require("./startup/database")()
 
 
-// get the port number from environment variables. if not defined, set it to 3000
+
+// Set the port number from environment variables. if not defined, set it to 3000
 const PORT = process.env.PORT || 3000
 
-// loading ssl key and certificate
+// Loading the SSL key and certificate from the 'ssl' directory
 const httpsOptions = {
     key: fs.readFileSync("./ssl/key.pem"),
     cert: fs.readFileSync('./ssl/cert.pem')
 };
 
-// An HTTPS listener that points to the Express app
+// Creating an HTTPS server that uses the SSL key and certificate,
+// and points to the Express app
 https
     .createServer(httpsOptions, app)
     .listen(PORT, ()=>{
